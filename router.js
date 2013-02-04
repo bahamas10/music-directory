a0221ef87885ee830e68d7ca50bea1ee4bfce642
Dave Eddy <dave@daveeddy.com>
2013-02-04 02:48:34 -0800

    adding styles, icon, new name, etc

--- router.js
+++ router.js
@@ -6,9 +6,10 @@ router.addRoute('/', require('./routes/index'));
 
 var _static = require('./routes/static');
 router.addRoute('/css/*?', _static);
+router.addRoute('/favicon.ico', _static);
+router.addRoute('/icon.html', _static);
 router.addRoute('/js/*?', _static);
 router.addRoute('/third-party/*?', _static);
-router.addRoute('/favicon.ico', _static);
 
 router.addRoute('/media/*?', require('./routes/media'));
 router.addRoute('/api/:action/:option?', require('./routes/api'));
